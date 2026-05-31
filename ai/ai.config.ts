5f2660a367bd4674ab454670dac1706226931846
Abhishek Mandavi <abhishekmandavi912@gmail.com>
2026-05-31 23:07:52 +0530

    commit

--- ai/ai.config.ts
+++ ai/ai.config.ts
@@ -1,9 +1,9 @@
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 
 export function getAgentModel() {
-    const provider = createOpenRouter({apiKey:process.env.OPENROUTER_API_KEY});
+    const provier = createOpenRouter({ apiKey: process.env.OPENROUTER_API_KEY });
 
     const modelId = process.env.OPENROUTER_DEFAULT_MODEL;
 
-    return provider(modelId);
+    return provier(modelId);
 }
