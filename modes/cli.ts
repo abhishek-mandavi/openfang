5c6238e0811bfc39ccbd3ea5b38274392dd7c7bd
Abhishek Mandavi <abhishekmandavi912@gmail.com>
2026-06-01 20:18:21 +0530

    commit

--- modes/cli.ts
+++ modes/cli.ts
@@ -7,29 +7,29 @@ import { runPlanMode } from "./plan/orchestrator";
 export async function runCliMode() {
     while (true) {
         const mode = await select({
-            message:"Choose CLI sub-mode",
-            options:[
-                {value:"agent" , label:"Agent Mode"},
-                {value:"plan", label:"Plan Mode"},
-                {value:"ask" , label:"Ask Mode"},
-                {value:"back", label: "Back to main menu"}
+            message: "Choose CLI sub-mode",
+            options: [
+                { value: "agent", label: "Agent Mode" },
+                { value: "plan", label: "Plan Mode" },
+                { value: "ask", label: "Ask Mode" },
+                { value: "back", label: "← Back to main menu" },
             ],
         });
 
-        if(isCancel(mode) || mode === "back") return;
+        if (isCancel(mode) || mode === "back") return;
 
-        if(mode === "agent"){
+        if (mode === "agent") {
             await runAgentMode()
         }
-        if(mode === "ask"){
+        if (mode === "ask") {
             await runAskMode()
         }
-        if(mode === "plan"){
-            await runPlanMode
+        if (mode === "plan") {
+            await runPlanMode()
         }
 
-        if(mode !== 'agent' && mode !== 'plan' && mode !== 'ask'){
-            console.log(chalk.red('\nThat mode is not implemented yet. \n'));
+        if (mode !== "agent" && mode !== "plan" && mode !== "ask") {
+            console.log(chalk.redBright("\nThat mode is not implemented yet.\n"));
         }
     }
 }
